1afd3e3b084df170b4beda6886f14942dd115570
Juan <50636966+juanfernandotirado@users.noreply.github.com>
2019-07-20 17:41:16 -0700

    Fixed minor errors in variables.scss

--- functions.php
+++ functions.php
@@ -133,6 +133,8 @@ function kiwe_scripts() {
 
 	wp_enqueue_script( 'kiwe-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
 
+	wp_enqueue_style( 'kiwe-fonts', 'https://fonts.googleapis.com/css?family=Lobster|Open+Sans:400,700|Source+Serif+Pro:400,700&display=swap' );
+
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
 	}
