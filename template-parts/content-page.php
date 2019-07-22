f4ec07dab6ea107187ddbd02082abdd1e53ad252
KarlaLGA <KarlaLGA@users.noreply.github.com>
2019-07-21 23:42:28 -0700

    Fix page bug

--- template-parts/content-page.php
+++ template-parts/content-page.php
@@ -30,6 +30,18 @@
 		) );
 		?>
 
+	<?php else : ?>
+
+	<?php
+		the_content();
+
+		wp_link_pages( array(
+			'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'kiwe' ),
+			'after'  => '</div>',
+		) );
+		?>
+
+
 		<?php endif; ?>
 	</div><!-- .entry-content -->
 
