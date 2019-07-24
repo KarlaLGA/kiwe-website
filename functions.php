e58b9c8c49b2efb0bf804d2a7fef87d1a9910e9f
manpreet058 <51723831+manpreet058@users.noreply.github.com>
2019-07-24 14:52:43 -0700

    header-footer update

--- functions.php
+++ functions.php
@@ -127,13 +127,18 @@ add_action( 'widgets_init', 'kiwe_widgets_init' );
  * Enqueue scripts and styles.
  */
 function kiwe_scripts() {
+	//google-font
+	wp_enqueue_style( 'kiwe-fonts', 'https://fonts.googleapis.com/css?family=Lobster|Open+Sans:400,700|Source+Serif+Pro:400,700&display=swap' );
+	
+	wp_enqueue_style( 'kiwe-font-awesome', 'https://use.fontawesome.com/releases/v5.7.2/css/all.css');
+
 	wp_enqueue_style( 'kiwe-style', get_stylesheet_uri() );
 
 	wp_enqueue_script( 'kiwe-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
 
 	wp_enqueue_script( 'kiwe-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
 
-	wp_enqueue_style( 'kiwe-fonts', 'https://fonts.googleapis.com/css?family=Lobster|Open+Sans:400,700|Source+Serif+Pro:400,700&display=swap' );
+	
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
--- header.php
+++ header.php
@@ -56,7 +56,7 @@
 		</div><!-- .site-branding -->
 
 		<nav id="site-navigation" class="main-navigation">
-			<!-- <button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'kiwe' ); ?></button> -->
+			<!--<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'kiwe' ); ?></button>  -->
 			<?php
 			wp_nav_menu( array(
 				'theme_location' => 'menu-main',
