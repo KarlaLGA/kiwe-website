5b0d969f0d6519c7303b6c8f92930d1e9c208721
Sunny Xue <sunny.sit26@gmail.com>
2019-07-29 16:24:51 -0700

    Add Parallax effect on About us

--- js/middle-menu.js
+++ js/middle-menu.js
@@ -1,10 +1,10 @@
-let section1ToTop =  $('.user-about-container').offset().top;
-console.log('H:',section1ToTop);
+// let section1ToTop =  $('.user-about-container').offset().top;
+// console.log('H:',section1ToTop);
 
 $( window ).scroll(function() {
 
     let wScroll = $(this).scrollTop();
-    console.log(wScroll);
+    // console.log(wScroll);
     
     
 
