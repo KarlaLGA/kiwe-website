036de069ff4f488f5af4599f145ef89db5233301
Sunny Xue <sunny.sit26@gmail.com>
2019-07-29 22:16:52 -0700

    Update functions.php to more human readable

--- functions.php
+++ functions.php
@@ -144,21 +144,19 @@ function kiwe_scripts() {
 
 	wp_enqueue_script( 'kiwe-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20190729', true );
 
-	if(is_page(8))
+	if(is_page('about'))
 	{
 	//Parallax Effect
 	wp_enqueue_script( 'kiwe-parallax', get_template_directory_uri() . '/js/parallax-effect.js', array('jquery'), '20190729', true );
-	
-
 	}
 
-	if(is_page(2))
+	if(is_home())
 	{
 	//Home Page Middle Menu 
 	wp_enqueue_script( 'kiwe-middle-menu', get_template_directory_uri() . '/js/middle-menu.js', array('jquery'), '20190729', true );
 	}
 
-	if(is_page(18))
+	if(is_page('rhome'))
 	{
 	//Restaurant Page Middle Menu
 	wp_enqueue_script( 'kiwe-middle-menu-restaurant', get_template_directory_uri() . '/js/middle-menu-restaurant.js', array('jquery'), '20190729', true );
