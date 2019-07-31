ab5c65672301a9c40730a1cad23f459b3ee511c1
Sunny Xue <sunny.sit26@gmail.com>
2019-07-31 12:35:00 -0700

    Hide Parallax

--- js/parallax-effect.js
+++ js/parallax-effect.js
@@ -1,32 +1,32 @@
-$( window ).scroll(function() {
+// $( window ).scroll(function() {
 
-    let wScroll = $(this).scrollTop();
-    // console.log(wScroll);
-    if($( window ).width()>760&&$(window).width<1000)
-    {
+//     let wScroll = $(this).scrollTop();
+//     // console.log(wScroll);
+//     if($( window ).width()>760&&$(window).width<1000)
+//     {
 
-    $('.about-us-header').css({
-        'transform' : 'translateY('+ wScroll +'%)'
-    })
+//     $('.about-us-header').css({
+//         'transform' : 'translateY('+ wScroll +'%)'
+//     })
 
-    $('.about-us-content').css({
-        'transform' : 'translateY(-'+ wScroll /90 +'%)'
-    })
+//     $('.about-us-content').css({
+//         'transform' : 'translateY(-'+ wScroll /90 +'%)'
+//     })
 
-    }
-    else if($(window).width()>=1000)
-    {   
-        console.log('helo');
-        $('.about-us-header').css({
-            'transform' : 'translateY('+ wScroll*1.1+'%)'
-        })
+//     }
+//     else if($(window).width()>=1000)
+//     {   
+//         console.log('helo');
+//         $('.about-us-header').css({
+//             'transform' : 'translateY('+ wScroll*1.1+'%)'
+//         })
     
-        $('.about-us-content').css({
-            'transform' : 'translateY(-'+ wScroll /45 +'%)'
-        })
-    }
+//         $('.about-us-content').css({
+//             'transform' : 'translateY(-'+ wScroll /45 +'%)'
+//         })
+//     }
    
 
 
 
-});
+// });
